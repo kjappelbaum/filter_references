2fdff8e75ac7082b0d0a39e32b060584fd234a2f
Kevin Jablonka <kevin.jablonka@epfl.ch>
2019-04-01 18:00:00 +0200

    Fixed some issues

--- scratch.py
+++ scratch.py
@@ -16,43 +16,40 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import pandas as pd 
 
+
 class WaterSpider(scrapy.Spider):
     name = "water"
     start_urls = ["http://chemsearch.kovsky.net/index.php"]
 
-    def __init__(self, reference=None):
+    def __init__(self, reference_list=None):
         self.download_delay = 0.25
-        self.reference = reference
+        self.reference_list = reference_list
     
 
     def parse(self, response):
-        formdata = {'q': self.reference}
-        yield FormRequest.from_response(response,
-                                formnumber=0,
-                                formdata=formdata,
-                                callback=self.parse1)
+        for reference in self.reference_list:
+            self.reference = reference 
+            formdata = {'q': reference}
+            yield FormRequest.from_response(response,
+                                    formnumber=0,
+                                    formdata=formdata,
+                                    callback=self.parse1)
 
     def parse1(self, response):
         is_redirect = response.xpath('/html/body/div/div/center/a[2]').extract()
         is_redirect = html.unescape(is_redirect[0])
-        print(is_redirect)
         url = re.findall(r'"([^"]*)"', is_redirect)[0]
-        print(url)
         self.parse_redirect_page(url)
-        #else:
-        #    ...
+
 
     def parse_redirect_page(self, url):
         session = HTMLSession()
         r = session.get(url)
         r.html.render(timeout=0, sleep=10)
         string = r.html.full_text
-        search = re.findall(r'water.isotherm', string)
-        result_list.append(
-            {
-                'reference' : self.reference, 
-                'result': search
-            })
+        search = re.findall(r'isotherm', string)
+        with open('result_log.txt', 'w+') as rl:
+            rl.write(self.reference + ' ' +  ''.join(search))
 
 if __name__ == '__main__':
 
@@ -60,16 +57,16 @@ if __name__ == '__main__':
         references = tl.readlines()
 
     result_list = []
+    reference_list = []
     for reference in references:
         print(reference)
-        reference = reference.strip('CrossRef').strip('CAS').strip('Pubmed')
-        reference = ''.join(reference.split(',')[-4:])
+        reference = reference.replace('CrossRef', '').replace('CAS', '').replace('PubMed', '')
+        reference = ','.join(reference.split(',')[-4:])
+        reference_list.append(reference)
 
-        settings = get_project_settings()
-        crawler = CrawlerProcess(settings)
-        spider = WaterSpider()
-        crawler.crawl(spider, reference)
-        crawler.start()
+        # settings = get_project_settings()
+    crawler = CrawlerProcess()
+    spider = WaterSpider()
+    crawler.crawl(spider, reference_list)
+    crawler.start()
     
-    df = pd.DataFrame(result_list)
-    print(df.head())
